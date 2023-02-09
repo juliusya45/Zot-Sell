9d3396a0d75baf9d60b44ed8e01d21c088344066
Julius Yang <68701101+juliusya45@users.noreply.github.com>
2023-02-08 23:59:31 -0800

    Listings are returned in chronological order desc

--- getlistings.js
+++ getlistings.js
@@ -1,16 +1,22 @@
 import { collection, getDocs } from "firebase/firestore";
+import { query, orderBy, limit } from "firebase/firestore";  
 export function handleListings(db)
 {
     return async function(req, res)
     {
+        const listingsRef = collection(db, "listings");
+        const q = query(listingsRef, orderBy("time", "desc"));
+        //console.log(q);
         //returns all listings in the database
         const allListings = {};
-        const querySnapshot = await getDocs(collection(db, "listings"));
+        //collection(db, "listings")
+        const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
         //console.log(doc.id, " => ", doc.data());
         allListings[doc.id] = doc.data();
         });
         res.send(allListings);
+
     }
 }
